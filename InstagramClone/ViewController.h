ab61219162884ad2fc1c85c2bad046b7bb5be294
Gael Rodriguez Gomez <gaelrg@fb.com>
2022-06-27 13:08:07 -0700

    Project and pod set up

---
+++ InstagramClone/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  InstagramClone
+//
+//  Created by Gael Rodriguez Gomez on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
